3d584523f3d6c109e94de1e476a05eb7310bea27
unknown <tr9836859@gmail.com>
2022-02-17 00:05:55 +0600

    for deplopying netlify

--- js/app.js
+++ js/app.js
@@ -30,10 +30,10 @@ document.getElementById("calculate").addEventListener("click", function (e) {
     parseFloat(income.value) - totalExpenses.textContent;
 
   // clear the input fields:
-  income.value = "";
-  foodCostText.value = "";
-  rentCostText.value = "";
-  clothesCostText.value = "";
+  //   income.value = "";
+  //   foodCostText.value = "";
+  //   rentCostText.value = "";
+  //   clothesCostText.value = "";
 });
 // for saving:
 
@@ -47,6 +47,36 @@ document.getElementById("save-btn").addEventListener("click", function () {
   const savingAmount = document.getElementById("saving-amount");
   savingAmount.textContent = parseFloat(income.value) * (saveAmountValue / 100);
 
-  //   clear the saveField:
-  save.value = "";
+  //   totalExpenses:
+  //o /for expenses:
+  const foodCostText = document.getElementById("food");
+
+  const foodCostTextValue = foodCostText.value;
+
+  const foodCostAmount = parseFloat(foodCostTextValue);
+
+  const rentCostText = document.getElementById("rent");
+  const rentCostTextValue = rentCostText.value;
+
+  const rentCostAmount = parseFloat(rentCostTextValue);
+
+  const clothesCostText = document.getElementById("clothes");
+
+  const clothesCostTextValue = clothesCostText.value;
+  const clothesCostAmount = parseFloat(clothesCostTextValue);
+
+  //   totalExpense:
+  const totalExpenses = document.getElementById("total-expenses");
+
+  totalExpenses.textContent =
+    foodCostAmount + rentCostAmount + clothesCostAmount;
+
+  const totalBalance = document.getElementById("total-balance");
+  totalBalance.textContent =
+    parseFloat(income.value) - totalExpenses.textContent;
+  console.log(totalBalance);
+  const remainingBalance = document.getElementById("remaining-balance");
+
+  remainingBalance.textContent =
+    totalBalance.textContent - savingAmount.textContent;
 });
